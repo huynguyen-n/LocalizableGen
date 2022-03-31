b07fbdb40d860f43ff23ecd72a42a767da8fc14a
Huy Nguyen <huy.ng1012@gmail.com>
2022-03-31 21:08:35 +0700

    update: package swift and resolved

--- Package.swift
+++ Package.swift
@@ -14,12 +14,14 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/apple/swift-argument-parser", from: "0.0.1")
+        .package(url: "https://github.com/apple/swift-argument-parser", from: "0.0.1"),
+        .package(url: "https://github.com/mtynior/ColorizeSwift.git", from: "1.5.0"),
+        .package(url: "https://github.com/huynguyen-n/google-auth-library-swift", from: "0.5.4"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(name: "LocalizableGen", dependencies: ["LocalizableGenCore"]),
-        .target(name: "LocalizableGenCore", dependencies: ["ArgumentParser"])
+        .target(name: "LocalizableGenCore", dependencies: ["ArgumentParser", "ColorizeSwift", "OAuth2"]),
     ]
 )
